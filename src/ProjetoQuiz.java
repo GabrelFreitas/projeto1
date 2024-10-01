b4a23d9205002137a1d7c0c1336e468ebd24c9b4
GabrelFreitas <trgabrielmf6@gmail.com>
2024-10-01 20:23:16 -0300

    Inicial

---
+++ src/ProjetoQuiz.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ProjetoQuiz {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Centro Universitário Alfredo Nascer - Unifan");
+        System.out.println("Aluno: Gabriel Henrique Moreira Freitas");
+        System.out.println("Disciplina: Algoitmo e Programação II");
+        System.out.println("Professor Breno Pimenta");
+        System.out.println("Olá, seja bem vindo ao Quiz!!");
+    }
+}
